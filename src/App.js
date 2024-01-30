ded183d851359990446b195c65b988b5e8b6a0d1
Ali Javadi <118165021+AliJavadi1997@users.noreply.github.com>
2024-01-30 23:48:00 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
 function App() {
-  return <div>Application</div>;
+  const tip = 0;
+  const bill = 0;
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage />
+      <SelectPercentage />
+      <Output tip={tip} bill={bill} />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput() {
+  return (
+    <div>
+      <label htmlFor="bill">How much was the bill?</label>
+      <input type="number" placeholder="Bill value" />
+    </div>
+  );
+}
+
+function SelectPercentage() {
+  return (
+    <div>
+      <label htmlFor="service">How did you like the service?</label>
+      <select id="service">
+        <option value={0}>Dissatisfied(0%)</option>
+        <option value={5}>It was OK(5%)</option>
+        <option value={10}>It was good(10%)</option>
+        <option value={20}>Absolutely amazing!(20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ tip, bill }) {
+  return <div>{`You pay $${tip + bill}: $${bill} + $${tip}`}</div>;
+}
+
+function Reset() {
+  return <button>Reset</button>;
 }
 
 export default App;
